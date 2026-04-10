64b1f12144285007c382029f827cd32286f12ed9
Dmytro <shevchenko.dmytro2@lll.kpi.ua>
2026-04-10 20:09:06 +0200

    Update vite.config.js

--- src/MapClient/vite.config.js
+++ src/MapClient/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/db-hwmap-src-map-client/',
   server: {
     host: '0.0.0.0',
-    port: 3000
-  }
+    port: 3000,
+  },
 })
